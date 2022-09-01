d251d3bfaa8d117252e7dfbaff4791c359897d0f
3405008677 <3405008677@qq.com>
2022-09-01 15:02:56 +0800

    路由问题

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,20 +1,103 @@
 import axios from 'axios'
 import { user } from '@/store'
+import { getToken } from './auth'
+import { ElNotification } from 'element-plus'
 
-
+// axios实例
 const request = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   timeout: 10000,
   headers: {}
 })
 
+// 加载动画
+let loading
+
 // 请求拦截器
-request.interceptors.request.use(() => {
-  const userStore = user()
-  if (userStore.token) {
-    
+request.interceptors.request.use(
+  (config) => {
+    // 请求前判断
+    const userStore = getToken()
+    if (userStore) {
+      config.headers['Authorization'] = `Bearer ${userStore}`
+    }
+    // 
+    loading = ElLoading.service({
+      text: '拼命加载中...',
+      background: 'rgba(178,215,173,1)'
+    })
+    return config
+  },
+  (err) => {
+    return Promise.reject(err)
   }
-})
+)
+
+// 响应拦截器
+request.interceptors.response.use(
+  response => {
+    if (loading) loading.close()
+    const { data } = response
+    if (data.success) return data
+    data.errCode
+  },
+  err => {
+    if (loading) loading.close()
+    const { status } = error.response
+    switch (status) {
+      case 0:
+        ElNotification({
+          title: "0",
+          message: '无法连接服务器',
+          type: 'warning',
+        })
+        break;
+      case 302:
+        ElNotification({
+          title: "302",
+          message: '登录过期',
+          type: 'warning',
+        })
+        break;
+      case 401:
+        ElNotification({
+          title: "401",
+          message: '未验证的用户',
+          type: 'warning',
+        })
+        break;
+      case 403:
+        ElNotification({
+          title: "403",
+          message: '资源拒绝访问',
+          type: 'warning',
+        })
+        break;
+      case 404:
+        ElNotification({
+          title: "404",
+          message: '服务资源不存在',
+          type: 'warning',
+        })
+        break;
+      case 500:
+        ElNotification({
+          title: "500",
+          message: '服务运行错误',
+          type: 'warning',
+        })
+        break;
+      case 504:
+        ElNotification({
+          title: "504",
+          message: '代理访问错误',
+          type: 'warning',
+        })
+        break;
+    }
+    return Promise.reject()
+  }
+)
 
 request.get = (url, params, headers = {}) => {
   return request({
