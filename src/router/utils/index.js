5610510530ce314d34a09f3fb6f107277d6d7371
似寻 <3405008677@qq.com>
2022-10-29 19:09:44 +0800

    10-29

--- src/router/utils/index.js
+++ src/router/utils/index.js
@@ -1,7 +1,5 @@
-import router from "@/router";
-
-const _import = (file) => () => import("/@/views/demo" + file + ".vue");
-
+// import router from "@/router";
+const _import = file => () => import.meta.glob('@/views/demo' + file + '.vue')
 /**
  * 修改Url添加参数
  * url：当前路由地址
@@ -9,47 +7,47 @@ const _import = (file) => () => import("/@/views/demo" + file + ".vue");
  * keyVal：参数value
  */
 export const changeURL = (url, key, keyVal) => {
-  let pattern = key + "=([^&]*)";
-  let replaceText = key + "=" + keyVal;
+  let pattern = key + '=([^&]*)'
+  let replaceText = key + '=' + keyVal
   if (url.match(pattern)) {
-    let tmp = "/(" + key + "=)([^&]*)/gi";
-    tmp = url.replace(eval(tmp), replaceText);
-    return tmp;
+    let tmp = '/(' + key + '=)([^&]*)/gi'
+    tmp = url.replace(eval(tmp), replaceText)
+    return tmp
   } else {
-    if (url.match("[?]")) {
-      return url + "&" + replaceText;
+    if (url.match('[?]')) {
+      return url + '&' + replaceText
     } else {
-      return url + "?" + replaceText;
+      return url + '?' + replaceText
     }
   }
-};
+}
 
 /**
  * 格式化router
  */
 export const formattingRouter = (router, father = {}) => {
-  let temp = [];
+  let temp = []
   for (let i = 0; i < router.length; i++) {
-    let v = {};
-    let meta = {};
-    v.path = router[i].path;
+    let v = {}
+    let meta = {}
+    v.path = router[i].path
     if (Object.keys(father).length) {
-      v.name = father.name + "/" + router[i].path;
+      v.name = father.name + '/' + router[i].path
     } else {
-      v.name = "/" + router[i].path;
+      v.name = '/' + router[i].path
     }
-    v.component = _import(v.name);
-    meta.title = router[i].name;
-    meta.elIcon = router[i].icon;
-    meta.keepAlice = false;
-    v.meta = meta;
+    v.component = _import(v.name)
+    meta.title = router[i].name
+    meta.elIcon = router[i].icon
+    meta.keepAlice = false
+    v.meta = meta
     if (router[i].sysMenus != undefined && router[i].sysMenus.length) {
-      v.redirect = v.name + "/" + router[i].sysMenus[0].path;
-      v.children = formattingRouter(router[i].sysMenus, v);
+      v.redirect = v.name + '/' + router[i].sysMenus[0].path
+      v.children = formattingRouter(router[i].sysMenus, v)
     } else {
-      if (router[i].parentId == "0") v.component = _import(v.name + v.name);
+      if (router[i].parentId == '0') v.component = _import(v.name + v.name)
     }
-    temp.push(v);
+    temp.push(v)
   }
-  return temp;
-};
+  return temp
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,13 @@
-import app from "./modules/app";
-import user from "./modules/user";
-import settings from "./modules/settings";
+import app from './modules/app'
+import user from './modules/user'
+import settings from './modules/settings'
 
-import { createPinia } from "pinia";
-const pinia = createPinia();
-export default pinia;
+import { createPinia } from 'pinia'
+const pinia = createPinia()
+export default pinia
 
 const appStore = app(pinia),
   userStore = user(pinia),
-  settingsStore = settings(pinia);
+  settingsStore = settings(pinia)
 
-export { appStore, userStore, settingsStore };
+export { appStore, userStore, settingsStore }
