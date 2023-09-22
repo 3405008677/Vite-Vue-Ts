5aa99dc1efe7338404930e76303d7af752a0f99a
彭于晏 <3405008677@qq.com>
2023-09-22 12:29:32 +0800

    update env file

--- src/router/modules/routerDate.ts
+++ src/router/modules/routerDate.ts
@@ -1,12 +1,10 @@
-export default 
-  {
-    path: 'home',
-    name: '/home',
-    meta: {
-      title: '主页',
-      needLogin: true,
-      keepAlive: false,
-    },
-    component: () => import('@/views/text/home.vue'),
-  }
-
+export default {
+  path: 'home',
+  name: '/home',
+  meta: {
+    title: '主页',
+    needLogin: true,
+    keepAlive: false,
+  },
+  component: () => import('@/views/demo/home/index.vue'),
+}
