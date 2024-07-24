e170a197d53c00ca8ae3488f78fc7efe774aacd7
unknown <pengqianjing@runmanage.com>
2024-07-24 10:08:08 +0800

    updata utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,28 @@
+// 判断字符串是否存在中文
+// import { containsChinese } from "@/utils"
+
+// 字符串转数组，根据 空格 回车 Tab
+// import { replaceArray } from "@/utils"
+
+// 数组去重
+// import { arrayNewSetData } from "@/utils"
+
+// 判断当前是环境否是为微信
+// import { isWXChrome } from "@/utils"
+
+// 获取当前日期
+// import { getNowFormatDate } from "@/utils"
+
+// 计算两个日期天数差的函数
+// import { daysBetween } from "@/utils"
+
+// 校验邮箱
+// import { checkEmail } from "@/utils"
+
+// 校验手机号
+// import { checkPhone } from "@/utils"
+
+
 export function getDateYear() {
   let time = new Date()
   return time.getFullYear()
@@ -82,4 +107,49 @@ export function getNowFormatDate() {
 export function isWXChrome() {
   let info = navigator.userAgent.toLowerCase()
   return !/wxwork/i.test(info) && /MicroMessenger/i.test(info)
+}
+
+/**
+ * 判断字符串是否存在中文
+ * @param str 需要检测的字符串
+ * @returns 返回 Boolean 类型
+ */
+export function containsChinese(str:string) {
+  // 匹配中文字符的正则表达式
+  // [\u4e00-\u9fa5] 是基本汉字区块
+  // \u3000-\u303f 是中日韩符号和标点
+  // \uf900-\ufaff 是CJK兼容扩展A
+  // \u3400-\u4dbf 是CJK扩展A
+  // \u{20000}-\u{2A6DF} 是CJK扩展B, CJK扩展C, CJK扩展D, CJK扩展E, CJK扩展F, CJK统一表意符号扩展A, CJK统一表意符号扩展B, CJK统一表意符号扩展C, CJK统一表意符号扩展D, CJK兼容表意符号补充
+  const regex = /[\u4e00-\u9fa5\u3000-\u303f\uf900-\ufaff\u3400-\u4dbf\u{20000}-\u{2A6DF}]+/u
+
+  // 判断字符串是否包含匹配到的中文字符
+  return regex.test(str)
+}
+
+/**
+ * 数组去重
+ * @param target 匹配的key
+ * @param arr 需要去重的数组——对象数组
+ */
+export function arrayNewSetData(target:string, arr:Array<any>):Array<any> {
+  // 使用 Set 来存储已经出现过的 id
+  let ids = new Set()
+  // 使用 filter 来过滤出具有唯一 id 的对象
+  return arr.filter(obj => {
+    if (!ids.has(obj[target])) {
+      // 如果 id 没有在 Set 中出现过，就添加到 Set 中，并返回 true（保留这个对象）
+      ids.add(obj[target])
+      return true
+    }
+    // 如果 id 已经在 Set 中出现过了，就返回 false（不保留这个对象）
+    return false
+  })
+}
+
+/**
+ *  字符串转数组，根据 空格 回车 Tab
+ */
+export function replaceArray(str:string):Array<string> {
+  return str.split(/[\t\n\r ]+/).map((item: string) => item.trim()).filter((item:string) => item !== '')
 }
